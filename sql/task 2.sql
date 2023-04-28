b9963ba9d2c336ac5879d46f405f0af686fbebdf
user <exchange.program2018@gmail.com>
2023-04-28 16:48:25 +0300

    query update

--- sql/task 2.sql
+++ sql/task 2.sql
@@ -1,50 +1,56 @@
-drop schema if exists consultant_info cascade;
-create SCHEMA IF NOT EXISTS consultant_info;
-SET SCHEMA 'consultant_info';
+drop schema if exists brand_info cascade;
+create SCHEMA IF NOT EXISTS brand_info;
+SET SCHEMA 'brand_info';
 
-create TABLE IF NOT EXISTS consultant_info.customers
+create TABLE IF NOT EXISTS brand_info.city
 (
     id       SERIAL       NOT NULL,
     name     VARCHAR(150) NOT NULL,
-    location VARCHAR(150) NOT NULL,
     PRIMARY KEY (id)
 );
 
-create TABLE IF NOT EXISTS consultant_info.consultants
+create TABLE IF NOT EXISTS brand_info.brands
 (
     id          SERIAL       NOT NULL,
     name        VARCHAR(150) NOT NULL,
-    customer_id INT          NOT NULL,
-    level       INT          NOT NULL,
-    age         INT          NOT NULL,
+    city_id INT          NOT NULL,
     PRIMARY KEY (id),
-    foreign key (customer_id) references consultant_info.customers (id) on delete cascade
+    foreign key (city_id) references brand_info.city (id) on delete cascade
 );
 
 
 
-insert into consultant_info.customers (name, location) values ('City EMEA', 'London'),
-                                              ('City NAM', 'New'),
-                                              ('Morgan Stanley NAM', 'New York'),
-                                              ('HSBC','London'),
-                                              ('Robobank', 'London');
+insert into brand_info.city (name)
+values ('London'),
+       ('Minsk'),
+       ('Warsaw'),
+       ('Paris');
 
 
-insert into consultant_info.consultants (name,customer_id,level,age) values ('James', 1, 3, 27),
-                                                            ('Cris', 2, 5, 38),
-                                                            ('Rama',3,5,41),
-                                                            ('Eve',1,4,39),
-                                                            ('John', 5,3,47),
-                                                            ('Mary',5,5,45),
-                                                            ('Bill',2,2,24);
+insert into brand_info.brands (name,city_id)
+values ('IKEA', 1),
+       ('Colgate', 2),
+       ('Samsung', 3),
+       ('Nike',3),
+       ('New Balance',1),
+       ('Adidas',4),
+       ('Lenovo',4),
+       ('JYSK',4);
 
 
-/*Task: Output the name of clients with corresponding number of consultants assigned to them that have at least two consultants*/
+
+
+
+/*Task: Output the name of cities with corresponding number of brands allocated in this city (at least two brands)*/
+
 
 
 /*Answer:*/
-/*select customers.name, count(c.id) from customers
-join consultants c on customers.id = c.customer_id
-group by customers.name
-having count(c.id) >= 2;  */
+/*select c.name, count(b.id) number
+from cities c
+         join brands b
+              on c.id = b.city_id
+group by c.name
+having count(b.id) >= 2
+order by c.name desc;*/
 
